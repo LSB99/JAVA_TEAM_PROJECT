67de4dd79f2040138c06f8d590884ab7022e60af
Lim Subin <elf9907@gmail.com>
2020-11-27 06:15:45 +0900

    이용요금 , 이용시간 체크 기능 구현

--- src/main/java/checkMoney/MoneyCheck.java
+++ src/main/java/checkMoney/MoneyCheck.java
@@ -1,4 +1,4 @@
-package net.shku.checkMoney;
+package checkMoney;
 
 import java.awt.Color;
 import java.awt.Container;
--- src/main/java/checkTime/TimerCheck.java
+++ src/main/java/checkTime/TimerCheck.java
@@ -1,4 +1,4 @@
-package net.skhu.checkTime;
+package checkTime;
 
 import java.awt.Color;
 import java.awt.Container;
